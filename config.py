f3c13fd9b04716abbe1ae4602e25ae80a470275f
Hanan Wiener <hanan888@gmail.com>
2018-09-06 16:13:16 +0300

    fixed parsing and improved logging

--- config.py
+++ config.py
@@ -2,12 +2,15 @@ import yaml
 
 publisher_config = None
 loader_path = None
+log_dict_config = None
 
 
 def create(config_file):
     global publisher_config
     global loader_path
+    global  log_dict_config
     with open(config_file, "r") as fdr:
         config = yaml.load(fdr)
     publisher_config = config["publisher"]
     loader_path = config["loader"]["path"]
+    log_dict_config = config["log"]
--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from pika_client.factory import create_persistent_async_publisher
-from config import publisher_config, loader_path
+import config
+import logging
+import logging.config
+import csv
 
 
 def parse_line(line):
-    parts = line.split(',')
+    final_parts = list(csv.reader([line]))[0]
+    sku = final_parts[3]
+    try:
+        sku = int(float(sku))
+    except ValueError:
+        raise ValueError("SKU is empty")
     return {
-        "product_name": parts[0],
-        "photo_url": parts[1],
-        "barcode": int(parts[2]) if parts[2] != "" else "",
-        "sku": int(parts[3]),
-        "price_cents": int(parts[4])if parts[4] != "" else "",
-        "producer": parts[5]
+        "product_name": final_parts[0],
+        "photo_url": final_parts[1],
+        "barcode": int(float(final_parts[2])) if final_parts[2] != "" else "",
+        "sku": int(float(final_parts[3])),
+        "price_cents": int(float(final_parts[4]))if final_parts[4] != "" else "",
+        "producer": final_parts[5]
     }
 
 
 if __name__ == "__main__":
 
-    publisher = create_persistent_async_publisher(publisher_config["connection"], publisher_config["route"],
-                                                  exchange=publisher_config["route"]["exchange"],
-                                                  routing_key=publisher_config["route"]["routing_key"])
+    config.create("config/config.yaml")
+
+    logging.config.dictConfig(config.log_dict_config)
+    log = logging.getLogger("loader")
+
+    publisher = create_persistent_async_publisher(config.publisher_config["connection"], config.publisher_config["route"],
+                                                  exchange=config.publisher_config["route"]["exchange"],
+                                                  routing_key=config.publisher_config["route"]["routing_key"])
     publisher.start()
-    with open(loader_path, "r") as fdr:
+    l_idx = 0
+    with open(config.loader_path, "r") as fdr:
         fdr.readline()
         for line in fdr:
-            publisher.send(parse_line(line))
+            l_idx +=1
+            try:
+                doc = parse_line(line)
+                publisher.send(doc)
+            except:
+                log.exception("{} {}".format(l_idx, line))
     publisher.flush()
     publisher.stop()
